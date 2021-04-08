e83ecbadc955bf444c0d8b7fe1cafe3ad3ed0c03
Felipe Moraes (WINDOWS) <felipeoliveiramoraes@hotmail.com>
2021-04-08 13:16:53 -0300

    Primeiro commit pelo Windows

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    background: var(--blue);
+`;
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+
+    padding: 2rem 1rem 12rem;  // 0 = top; 1rem = left and right; 10rem = bottom;
+    display: flex; // Display flexivel. Padrão horizontal.
+    align-items: center;
+    justify-content: space-between; // Espaçamento entre os itens.
+
+    button {
+        font-size: 1rem;
+        color: #FFF;
+        background: var(--blue-light);
+        border: 0;
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+
+        transition: filter 0.2s;    // Toda vez que o "filter" for alterado, sia transição será de 0.2s.
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
+`;
